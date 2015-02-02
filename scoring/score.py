eb79e57b55d2a956f81dfbaf9ff97bd7faf0b374
Alistair Lynn <arplynn@gmail.com>
2015-02-02 20:43:45 +0000

    Use a flat key to identify flag count
    See Trac #2700

--- scoring/score.py
+++ scoring/score.py
@@ -3,5 +3,5 @@ class Scorer:
         self._scoresheet = scoresheet
 
     def calculate_scores(self):
-        return {tla: len(data['flags']) for tla, data in self._scoresheet.items()}
+        return {tla: data['flags'] for tla, data in self._scoresheet.items()}
 
