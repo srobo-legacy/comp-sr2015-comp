c31703d504fcc2afa41217bd0ed5f7aea4c38d6f
Peter Law <PeterJCLaw@gmail.com>
2015-03-08 22:45:55 +0000

    Fix #2742: Add validation to the scoring script.
    Change-Id: I07bd8020b0612a960464853a6a9ff468757255ab

--- scoring/score.py
+++ scoring/score.py
@@ -1,7 +1,45 @@
+
+class InvalidScoresheetException(Exception):
+    pass
+
+MAX_FLAGS = 5
+
 class Scorer:
     def __init__(self, scoresheet):
         self._scoresheet = scoresheet
 
     def calculate_scores(self):
-        return {tla: data['flags'] for tla, data in self._scoresheet.items()}
+        scores = {}
+        for tla, data in self._scoresheet.items():
+            flags = data['flags']
+            self._check_num_flags(tla, flags)
+            scores[tla] = flags
+        return scores
 
+    def validate(self, extra):
+        total_flags = 0
+
+        for tla, data in self._scoresheet.items():
+            flags = data['flags']
+            self._check_num_flags(tla, flags)
+            total_flags += flags
+
+        if extra is not None:
+            unclaimed_flags = extra['unclaimed_flags']
+            self._check_num_flags('unclaimed_flags', unclaimed_flags)
+            total_flags += unclaimed_flags
+
+        if total_flags != MAX_FLAGS:
+            msg = "Wrong overall number of flags (expecting {0}, got {1})" \
+                    .format(total_flags, MAX_FLAGS)
+            raise InvalidScoresheetException(msg)
+
+    def _check_num_flags(self, who, flags):
+        if flags > MAX_FLAGS:
+            msg = "{0} has too many flags ({1}; max: {2})" \
+                    .format(who, flags, MAX_FLAGS)
+            raise InvalidScoresheetException(msg)
+
+        if flags < 0:
+            msg = "{0} has negative flags! ({1})".format(who, flags)
+            raise InvalidScoresheetException(msg)
---
+++ scoring/tests/test_ctf_scoring.py
@@ -0,0 +1,93 @@
+
+# Path hackery
+import os.path
+import sys
+ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, ROOT)
+
+from score import Scorer, InvalidScoresheetException
+
+def test_scores():
+    input_data = {
+        'ABC': {'flags': 0},
+        'DEF': {'flags': 1},
+        'GHI': {'flags': 2}
+    }
+    expected = {
+        'ABC': 0,
+        'DEF': 1,
+        'GHI': 2
+    }
+
+    scorer = Scorer(input_data)
+    scores = scorer.calculate_scores()
+
+    assert expected == scores, "Wrong scores!"
+
+def test_bad_scores():
+    input_data = {
+        'ABC': {'flags': -1},
+        'DEF': {'flags': 1},
+        'GHI': {'flags': 2}
+    }
+
+    try:
+        scorer = Scorer(input_data)
+        scores = scorer.calculate_scores()
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when scores are invalid"
+
+def test_bad_scores_validation():
+    input_data = {
+        'ABC': {'flags': -1},
+        'DEF': {'flags': 1},
+        'GHI': {'flags': 2}
+    }
+
+    try:
+        scorer = Scorer(input_data)
+        scores = scorer.calculate_scores()
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when scores are invalid"
+
+def test_bad_unclaimed_validation():
+    input_data = {
+        'ABC': {'flags': 4},
+        'DEF': {'flags': 1},
+        'GHI': {'flags': 2}
+    }
+    extra = {'unclaimed_flags': -3}
+
+    try:
+        scorer = Scorer(input_data)
+        scores = scorer.validate(extra)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when scores are invalid"
+
+def test_validation_too_many_flags():
+    input_data = {
+        'ABC': {'flags': 0},
+        'DEF': {'flags': 1},
+        'GHI': {'flags': 2}
+    }
+    extra = {'unclaimed_flags': 42}
+    expected = {
+        'ABC': 0,
+        'DEF': 1,
+        'GHI': 2
+    }
+
+    try:
+        scorer = Scorer(input_data)
+        scores = scorer.validate(extra)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when scores are invalid"
+
